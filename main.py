b2de6137a5aabcf02397fa9854231849e549ec55
Gabriel Sanchez <gabriel002002@gmail.com>
2026-05-13 18:12:12 -0600

    preparar creación de directorio data

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 
 project_path = os.getcwd() + "/"
-db_path = project_path + "data/clima.db"
+data_path = project_path + "data/"
+
+db_path = data_path + "clima.db"
+csv_path = data_path + "clima.csv"
 api_endpoint = """https://api.open-meteo.com/v1/forecast?latitude=19.43&longitude=-99.13&hourly=temperature_2m,precipitation&past_days=7&forecast_days=0"""
 
 if __name__ == "__main__":
+    os.makedirs(data_path, exist_ok=True) # creando directorio data si no existe
     from src.pipeline import pipeline
-    pipeline = pipeline(db_path, api_endpoint)
+    pipeline = pipeline(db_path, csv_path, api_endpoint)
     pipeline.run()
